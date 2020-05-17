e62a29def90ccf9c9c2f66c0d7e14a4a368ae5ee
omuomugin <omuomugin@out-box.dev>
2020-05-17 14:38:49 +0900

    refactor: use deffered and awaitAll

--- src/main/kotlin/infra/command/GitHubLabelAddCommandServiceImpl.kt
+++ src/main/kotlin/infra/command/GitHubLabelAddCommandServiceImpl.kt
@@ -6,6 +6,7 @@ import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
 import domain.model.Label
 import infra.converter.LabelConverter
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
@@ -18,7 +19,7 @@ class GitHubLabelAddCommandServiceImpl : GitHubLabelAddCommandService {
         labels: List<Label>
     ) {
         runBlocking {
-            LabelConverter.convertToInfraModel(labels)
+            val deferreds: List<Deferred<Unit>> = LabelConverter.convertToInfraModel(labels)
                 .map { label ->
                     async {
                         val (_, _, result) = Fuel.post("https://api.github.com/repos/${owner}/${repoName}/labels")
@@ -32,7 +33,9 @@ class GitHubLabelAddCommandServiceImpl : GitHubLabelAddCommandService {
                             { error -> throw Exception("${error.message}") }
                         )
                     }
-                }.awaitAll()
+                }
+
+            deferreds.awaitAll()
         }
     }
 }
--- src/main/kotlin/infra/command/GitHubLabelDeleteCommandServiceImpl.kt
+++ src/main/kotlin/infra/command/GitHubLabelDeleteCommandServiceImpl.kt
@@ -5,6 +5,7 @@ import com.github.kittinunf.fuel.core.extensions.authentication
 import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
 import domain.model.Label
 import infra.converter.LabelConverter
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
@@ -12,9 +13,9 @@ import kotlinx.coroutines.runBlocking
 class GitHubLabelDeleteCommandServiceImpl : GitHubLabelDeleteCommandService {
     override fun deleteLabels(owner: String, repoName: String, token: String, labels: List<Label>) {
         runBlocking {
-            LabelConverter.convertToInfraModel(labels)
+            val deferreds: List<Deferred<Unit>> = LabelConverter.convertToInfraModel(labels)
                 .map { label ->
-                    async {
+                    async() {
                         val (_, _, result) = Fuel.delete("https://api.github.com/repos/${owner}/${repoName}/labels/${label.name}")
                             .authentication()
                             .bearer(token)
@@ -25,7 +26,9 @@ class GitHubLabelDeleteCommandServiceImpl : GitHubLabelDeleteCommandService {
                             { error -> throw Exception("${error.message}") }
                         )
                     }
-                }.awaitAll()
+                }
+
+            deferreds.awaitAll()
         }
     }
 
